f2ee97f2e40fe9c491fdff3343aa756461dd2187
Stephany Eloise <stephany.eloise@hotmail.com>
2022-09-14 21:31:19 -0300

    Estruturas Condicionais

---
+++ 03conversao-de-tipos.py
@@ -0,0 +1,18 @@
+# Conversão de Tipos
+
+idade = '27'
+
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+
+print(idade_inteira, type(idade_inteira))
+
+# int()
+# str()
+# float()
+# bool()
+
+altura = float(input('Informe  sua altura: '))
+
+print(altura, type(altura))
